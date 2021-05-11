8c0058fe4e535a4a854933e9dff7ce6ed06c1e2b
심재욱 <78770230+SimJaeuk@users.noreply.github.com>
2021-05-11 17:23:59 +0900

    Add files via upload

---
+++ PyQt/Chart/overview_0.py
@@ -0,0 +1,19 @@
+import os
+from tkinter import *
+
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QWidget
+
+class OverviewWidget(QWidget):
+    def __init__(self, parent=None, ticker="BTC"):
+        super().__init__(parent)
+        uic.loadUi(file=resource_path("resource/overview.ui", self))
+
+if __name__ == "__main__":
+    import sys
+    from PyQt5.QtWidgets import QApplication
+    app = QApplication(sys.argv)
+    ob = OverviewWidget()
+    ob.show()
+    exit(app.exec_())
